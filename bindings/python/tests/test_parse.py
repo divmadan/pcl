1625441c9298c24dbd45d6142f7367d39819b583
Divyanshu Madan <iamdm99@gmail.com>
2020-07-05 16:28:36 +0530

    add test_node_in_this_file

--- bindings/python/tests/test_parse.py
+++ bindings/python/tests/test_parse.py
@@ -1,9 +1,30 @@
-from context import scripts
+# from context import scripts
 import scripts.parse as parse
 
-import pytest
+import clang.cindex as clang
 
 
-unsaved_files = (("test_parse.cpp", "void function() {;}"),)
+def test_node_in_this_file_true():
+    def recursive_call(cursor):
+        for child in cursor.get_children():
+            assert parse.node_in_this_file(child, tu.spelling) == True
+            recursive_call(cursor=child)
 
-tu = parse.clang.TranslationUnit.from_source(filename="test_parse.cpp", unsaved_files=unsaved_files)
+    unsaved_files = (("file.cpp", "void function() {}"),)
+    tu = clang.TranslationUnit.from_source(filename="file.cpp", unsaved_files=unsaved_files)
+    recursive_call(cursor=tu.cursor)
+
+
+def test_node_in_this_file_false_none():
+    def recursive_call(cursor):
+        for child in cursor.get_children():
+            if child.location.file:
+                assert parse.node_in_this_file(child, tu.spelling) == False
+            else:
+                assert parse.node_in_this_file(child, tu.spelling) == None
+            recursive_call(cursor=child)
+
+    unsaved_files = (("file.cpp", "#include<algorithm>"),)
+    tu = clang.TranslationUnit.from_source(filename="file.cpp", unsaved_files=unsaved_files)
+    recursive_call(cursor=tu.cursor)
+
